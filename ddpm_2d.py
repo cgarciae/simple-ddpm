c60dcdf00b4725bf51155215e6b893c68708d8e3
Cristian Garcia <cgarcia.e88@gmail.com>
2022-09-15 14:06:52 -0500

    remove utils

--- ddpm_2d.py
+++ ddpm_2d.py
@@ -19,8 +19,6 @@ from dataclasses import dataclass
 
 from IPython import get_ipython
 
-from ddpm_utils import update_config_from_args
-
 
 @dataclass
 class Config:
@@ -39,16 +37,32 @@ class Config:
 
 config = Config()
 
+# %%
+
 if not get_ipython():
-    config = update_config_from_args(config)
+    import sys
+
+    from absl import flags
+    from ml_collections import config_flags
+
+    config_flag = config_flags.DEFINE_config_dataclass("config", config)
+    flags.FLAGS(sys.argv)
+    config = config_flag.value
+
+
+def show_interactive():
+    if not get_ipython():
+        plt.ion()
+        plt.pause(1)
+        plt.ioff()
+    else:
+        plt.show()
 
 
 # %%
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs, make_moons
 from sklearn.preprocessing import MinMaxScaler
-
-from ddpm_utils import show_interactive
 
 
 def get_data(dataset: str = "moons"):
@@ -297,8 +311,63 @@ for epoch in range(config.epochs):
 
 
 # %%
-from ddpm_utils import plot_trajectory_2d
 
+from base64 import b64encode
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+from einop import einop
+from IPython.display import HTML, display
+from matplotlib import animation
+
+
+def plot_trajectory_2d(
+    xs: np.ndarray,
+    interval: int = 10,
+    repeat_delay: int = 1000,
+    step_size: int = 1,
+    end_pad: int = 500,
+):
+
+    xs = xs[::step_size]
+
+    # replace last sample to create a 'pause' effect
+    pad_end = einop(xs[-1], "... -> batch ...", batch=end_pad)
+    xs = np.concatenate([xs, pad_end], axis=0)
+
+    N = len(xs)
+
+    fig = plt.figure()
+    scatter = plt.scatter(xs[0][:, 0], xs[0][:, 1], s=1)
+
+    def animate(i):
+        scatter.set_offsets(xs[i])
+        return [scatter]
+
+    anim = animation.FuncAnimation(
+        fig,
+        animate,
+        init_func=lambda: animate(0),
+        frames=np.linspace(0, N - 1, N, dtype=int),
+        interval=interval,
+        repeat_delay=repeat_delay,
+        blit=True,
+    )
+
+    if get_ipython():
+        with TemporaryDirectory() as tmpdir:
+            img_name = Path(tmpdir) / f"diffusion.gif"
+            anim.save(str(img_name), writer="pillow", fps=60)
+            image_bytes = b64encode(img_name.read_bytes()).decode("utf-8")
+
+        display(HTML(f"""<img src='data:image/gif;base64,{image_bytes}'>"""))
+    else:
+        pass
+
+    return anim
+
+
+# %%
 
 x = jax.random.uniform(key, (1000, 2), minval=-1, maxval=1)
 ts = jnp.arange(config.num_steps, 0, -1)
@@ -311,9 +380,36 @@ else:
         xs, step_size=2, interval=100, repeat_delay=1000, end_pad=0
     )
 
+# %%
+from typing import Any, Callable
+
+
+def plot_density(model_fn: Callable[[Any, Any], Any], ts):
+    x = jnp.linspace(-1, 1, 100)
+    y = jnp.linspace(-1, 1, 100)
+    xx, yy = jnp.meshgrid(x, y)
+    X = jnp.stack([xx, yy], axis=-1)
+
+    def mass_fn(x, t):
+        t_ = jnp.full((1,), t)
+        x_ = x[None]
+        noise_hat = model_fn(x_, t_)
+        magnitud = jnp.linalg.norm(noise_hat, axis=-1, keepdims=False)
+        mass = jnp.exp(-magnitud)
+        return mass[0]
+
+    mass_fn = jax.jit(
+        jax.vmap(
+            jax.vmap(jax.vmap(mass_fn, in_axes=(0, None)), in_axes=(0, None)),
+            in_axes=(None, 0),
+            out_axes=-1,
+        )
+    )
+    mass = mass_fn(X, ts).mean(axis=-1)
+    plt.contourf(xx, yy, mass, levels=100)
+
 
 # %%
-from ddpm_utils import plot_density
 
 plt.figure()
 plot_density(
